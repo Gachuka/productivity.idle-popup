b4de1dfc4b65de327890474a3db813d8cffd360d
Li Yu <gachuka@gmail.com>
2022-09-17 12:35:35 -0400

    Add base functionality

--- src/component/TypedText/TypedText.js
+++ src/component/TypedText/TypedText.js
@@ -1,8 +1,39 @@
 import './TypedText.scss'
 
 function TypedText() {
+  
+  // const [ typed, setTyped ] = useState("")
+  const [ firstString, setFirstString ] = useState("")
+  const [ secondString, setSecondString ] = useState("")
+  const [ thirdString, setThirdString ] = useState("")
+  let typedString = ""
+
+  const downHandler = (event) => {
+    typedString += event.key
+    // setTyped(typedString)
+    setFirstString(typedString.slice(-15))
+    setSecondString(typedString.slice(-45, -15))
+    setThirdString(typedString.slice(-75, -45))
+    console.log(typedString)
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", downHandler);
+    // window.addEventListener("keyup", upHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      // window.removeEventListener("keyup", upHandler);
+    };
+  }, []);
+
   return (
-    <></>
+    <>
+      {/* <div>{typed}</div> */}
+      <div>{thirdString}</div>
+      <div>{secondString}</div>
+      <div>{firstString}</div>
+    </>
   )
 }
 
