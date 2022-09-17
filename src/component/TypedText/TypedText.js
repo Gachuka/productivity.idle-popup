d5a96bbc5ed1d03258592233332e1ce230986535
Li Yu <gachuka@gmail.com>
2022-09-17 13:55:49 -0400

    Implement Axios get for popup

--- src/component/TypedText/TypedText.js
+++ src/component/TypedText/TypedText.js
@@ -1,14 +1,18 @@
 import './TypedText.scss'
 import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const API_URL = "http://localhost:7878"
+console.log(API_URL)
 
 function TypedText() {
   
-  // const [ typed, setTyped ] = useState("")
+  const [ typed, setTyped ] = useState("")
   const [ firstString, setFirstString ] = useState("")
   const [ secondString, setSecondString ] = useState("")
   const [ thirdString, setThirdString ] = useState("")
   const [ fourthString, setFourthString ] = useState("")
-  let typedString = ""
+  let typedString = typed
 
   const downHandler = (event) => {
     typedString += event.key
@@ -24,11 +28,22 @@ function TypedText() {
     window.addEventListener("keydown", downHandler);
     // window.addEventListener("keyup", upHandler);
     // Remove event listeners on cleanup
+
+    axios.get(API_URL).then((response) => {
+      console.log('Axios Got')
+      setTyped(response.data)
+    }).then(() => {
+      setFirstString(typedString.slice(-15))
+      setSecondString(typedString.slice(-45, -15))
+      setThirdString(typedString.slice(-75, -45))
+      setFourthString(typedString.slice(-105, -75))
+    });
+
     return () => {
       window.removeEventListener("keydown", downHandler);
       // window.removeEventListener("keyup", upHandler);
     };
-  }, []);
+  }, [typed]);
 
   return (
     <>
