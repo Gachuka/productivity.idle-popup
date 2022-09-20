e8cec8b78523d0f6a7a32ce508f48443e564ae21
Li Yu <gachuka@gmail.com>
2022-09-19 22:10:36 -0400

    Basic UI for upgrade, Implemented buy buttons

--- src/pages/UpgradePage/UpgradePage.js
+++ src/pages/UpgradePage/UpgradePage.js
@@ -7,6 +7,8 @@ const API_URL = "http://localhost:7878"
 function UpgradePage({data}) {
 
   let upgrade1Number = data.upgrade_1
+  let upgrade2Number = data.upgrade_2
+  let upgrade3Number = data.upgrade_3
   const navigate = useNavigate()
 
   const handleBack = () => {
@@ -19,18 +21,39 @@ function UpgradePage({data}) {
   }
   const handleUpgrade2 = () => {
     console.log('Upgrade Two Clicked')
+    axios.put(API_URL, {upgrade_2: upgrade2Number += 1})
   }
   const handleUpgrade3 = () => {
     console.log('Upgrade Three Clicked')
+    axios.put(API_URL, {upgrade_3: upgrade3Number += 1})
   }
 
   return (
     <section className='upgrade__container'>
-      <h1>UpgradePage</h1>
+      <h1 className='upgrade__header'>UpgradePage</h1>
       <div className='upgrade__options option'>
-        <div className='option__1' onClick={handleUpgrade1}>500</div>
-        <div className='option__2' onClick={handleUpgrade2}>1000</div>
-        <div className='option__3' onClick={handleUpgrade3}>750</div>
+        <div className='option__container'>
+          <div className='option__cta'>
+            <span>Magic Finger:</span>
+            <div className='option__btn' onClick={handleUpgrade1}>500</div>
+          </div>
+          <p>Add one extra letter per input</p>
+        </div>
+        <div className='option__container'>
+          <div className='option__cta'>
+            <span>Magic Double:</span>
+            <div className='option__2' onClick={handleUpgrade2}>1000</div>
+          </div>
+          <p>Doubles every input</p>
+        </div>
+        <div className='option__container'>
+          <div className='option__cta'>
+            <span>Type Bot:</span>
+            <div className='option__3' onClick={handleUpgrade3}>750</div>
+          </div>
+          <p>Types one letter per interval</p>
+        </div>
+        
       </div>
       <div className='upgrade__back' onClick={handleBack}>Back</div>
     </section>
